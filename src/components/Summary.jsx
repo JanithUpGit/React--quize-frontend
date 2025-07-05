075aeef72568174300764dd67494d036e73ab0c9
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-05 21:22:35 +0530

    fix: Update Summary component to calculate and display accurate quiz statistics

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,32 +1,48 @@
+import { use } from 'react';
 import quizCompleteImage from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js'
 export default function Summary({ userAnswers }) {
-    console.log(userAnswers);
+    
+    const skippedAnswers = userAnswers.filter(answer => answer === null);
+    const correctAnswers =  userAnswers.filter((answer,index) => answer === QUESTIONS[index].answers[0] );
+    const incorrectAnswers = userAnswers.filter((answer,index) => answer !== QUESTIONS[index].answers[0] );
+
+
     return <>
         <div id="summary">
             <img src={quizCompleteImage} alt='Trophy icon' />
             <h2>Quiz Completed</h2>
             <div id="summary-stats">
                 <p>
-                    <span className='number'>10%</span>
+                    <span className='number'>{(skippedAnswers.length/userAnswers.length * 100).toFixed(0)}%</span>
                     <span className='text'>skipped</span>
                 </p>
                 <p>
-                    <span className='number'>10%</span>
+                    <span className='number'>{(correctAnswers.length/userAnswers.length * 100).toFixed(0)}%</span>
                     <span className='text'>answered correctly</span>
                 </p>
                 <p>
-                    <span className='number'>10%</span>
+                    <span className='number'>{(incorrectAnswers.length/userAnswers.length * 100).toFixed(0)}%</span>
                     <span className='text'>answered incorrectly</span>
                 </p>
             </div>
             <ol>
                 {userAnswers.map((answer, index) => {
+                    let cssClass = 'user-answer'
+
+                    if(answer === null){
+                        cssClass += ' skipped';
+                    }else if(answer === QUESTIONS[index].answers[0]){
+                        cssClass += ' correct';
+                    }else{
+                        cssClass += ' wrong';
+                    }
+
                     return (
                         <li key={answer}>
                             <h3>{index + 1}</h3>
                             <p className='question'>{QUESTIONS[index].text}</p>
-                            <p className='user-answer'>{answer}</p>
+                            <p className={cssClass}>{answer ?? 'Skipped'}</p>
                         </li>
                     );
                 })}
