d4c739fe3564bbe9aae85bef86551970907f04db
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-05 13:55:57 +0530

    feat: Add quiz application components and styling
    - Implement Header component with logo and title.
    - Create QuestionTimer component to manage question timing.
    - Develop Quiz component to handle quiz logic and user answers.
    - Add questions data structure for quiz content.
    - Style application with CSS for a cohesive look and feel.
    - Set up main entry point for React application using Vite.

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        const timer = setTimeout(onTimeout, timeout);
+        console.log("setTimeout")
+        return () => {
+            clearTimeout(timer);
+            console.log("clearTimeout")
+        };
+    }, [timeout, onTimeout])
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
+        }, 100)
+        console.log("setintervle")
+
+        return ()=>{
+            clearInterval(interval);
+            console.log("clearInterval")
+        }
+    }, [timeout])
+
+
+
+    return <>
+
+        <progress id='question-time' max={timeout} value={remainingTime}> </progress>
+
+    </>
+}
